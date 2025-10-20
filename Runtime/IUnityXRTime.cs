76762bb4d89abfcf11e833ac0060c5043be1bfb3
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2025-10-20 14:15:02 +1000

    Add IUnityXRTime

---
+++ Runtime/IUnityXRTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UnityEngine.NativeAPI
+{
+    [StructLayout(LayoutKind.Sequential)]
+    [Guid("650C79F9-6492-40B7-9F89-DF9F47DDD42F")]
+    public unsafe struct IUnityXRTime : IUnityInterface
+    {
+        public delegate* unmanaged[Stdcall]<double, void> SetPredictedRenderTime;
+
+        readonly void IUnityInterface.InternalImplementationOnly()
+        {
+        }
+    }
+}
